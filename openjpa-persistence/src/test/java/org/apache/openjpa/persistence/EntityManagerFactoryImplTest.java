612b77c501e27fd9ac6c078c0c7955f009285fe0
Simone Staccone <simone.staccone@virgilio.it>
2023-06-09 13:40:00 +0200

    First dummy test with Jacoco and PIT working

---
+++ openjpa-persistence/src/test/java/org/apache/openjpa/persistence/EntityManagerFactoryImplTest.java
@@ -0,0 +1,32 @@
+package org.apache.openjpa.persistence;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class EntityManagerFactoryImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getBroker() {
+        EntityManagerFactoryImpl entityManagerFactory = new EntityManagerFactoryImpl();
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void getEntityManagerFactory() {
+    }
+
+    @Test
+    void pushFetchPlan() {
+    }
+}
